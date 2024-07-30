a147e5f7925be90cd85d1d8ba8c3902060aca95b
Wiktor <110833197+droperxyz@users.noreply.github.com>
2024-07-30 13:18:46 +0200

    Create 3.py

---
+++ z3/3.py
@@ -0,0 +1,21 @@
+file = open('dane3.txt', 'r')
+ranges = []
+longest = 0
+
+for line in file:
+    line = line.split()
+    ranges.append(line)
+
+for i in range(len(ranges)):
+    first_range = ranges[i]
+    longest_temp = 1
+    for j in range(1, len(ranges)):
+        next_range = ranges[j]
+        if first_range[0] <= next_range[0] and first_range[1] <= next_range[1]:
+            longest_temp += 1
+        else:
+            if longest_temp > longest:
+                longest = longest_temp
+            break
+
+print(longest)
