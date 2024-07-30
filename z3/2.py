af121f6137af06cffeda74488ab9a35834b3f288
Wiktor <110833197+droperxyz@users.noreply.github.com>
2024-07-30 12:35:48 +0200

    Create 2.py

---
+++ z3/2.py
@@ -0,0 +1,18 @@
+file = open('dane3.txt', 'r')
+ranges = []
+
+def GetRangeLen(a, b):
+    return b - a + 1
+
+for i in range(2024):
+    ranges.append(0)
+
+
+for line in file:
+    line = line.split()
+    a = int(line[0])
+    b = int(line[1])
+    range_len = GetRangeLen(a, b)
+    ranges[range_len] += 1
+
+print(ranges.index(max(ranges)))
